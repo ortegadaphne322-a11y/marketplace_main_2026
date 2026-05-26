e9c51d7ca0e8ca3dc67d98188a5a4895a4234b9a
ortegadaphne322-a11y <ortegadaphne322@gmail.com>
2026-05-26 16:47:07 -0700

    modifique settings.py

--- marketplace_main/settings.py
+++ marketplace_main/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store'
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'store.User'
